da94983a87c95a50546653e0eabde975f1f5e161
Andrey Yakovlev <tigra9922@gmail.com>
2020-03-31 19:03:36 +1000

    плюс 3 пример

---
+++ primer3real/primer3real.cpp
@@ -0,0 +1,19 @@
+﻿#include <windows.h>
+WNDPROC OldWndProc; //переменная для хранения адреса старой оконной процедуры
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wparam, LPARAM lparam) {
+	switch (uMsg) {
+	case WM_LBUTTONUP: MessageBeep(0xFFFFFFFF); break;
+	default: return CallWindowProc(OldWndProc, hwnd, uMsg, wparam, lparam);
+	}
+	return (0);
+}
+int WINAPI DllMain(HINSTANCE hInstance, HANDLE hModule, DWORD dwReason, LPVOID lpReserved) {
+	HWND hwnd;
+	switch (dwReason) {
+	case DLL_PROCESS_ATTACH:
+		hwnd = CreateWindow(L"BUTTON", L"", 0, 0, 0, 0, 0, NULL, NULL, hInstance, NULL);
+		OldWndProc = (WNDPROC)SetClassLong(hwnd, GCL_WNDPROC, (LONG)WndProc);
+	DestroyWindow(hwnd);
+	}
+	return TRUE;
+}
